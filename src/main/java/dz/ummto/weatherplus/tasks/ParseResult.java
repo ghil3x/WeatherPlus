9b7b48d9aea6c026f6632cc5a97bc3bf99c78708
RAMDANI Aghiles <ghilex74@gmail.com>
2016-10-20 06:37:53 +0200

    Add files via upload

---
+++ src/main/java/dz/ummto/weatherplus/tasks/ParseResult.java
@@ -0,0 +1,3 @@
+package dz.ummto.weatherplus.tasks;
+
+public enum ParseResult {OK, JSON_EXCEPTION, CITY_NOT_FOUND}
---
+++ src/main/java/dz/ummto/weatherplus/tasks/TaskOutput.java
@@ -0,0 +1,8 @@
+package dz.ummto.weatherplus.tasks;
+
+public class TaskOutput {
+    // Indicates result of parsing server response
+    ParseResult parseResult;
+    // Indicates result of background task
+    TaskResult taskResult;
+}
